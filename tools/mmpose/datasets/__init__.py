13a2957dc15f9ac0ae4f1fc2a46c1ee71806f605
Hao Zhang <49357570+zhanghao5201@users.noreply.github.com>
2024-04-29 16:58:09 +0800

    Create __init__.py

---
+++ tools/mmpose/datasets/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .dataset_info import DatasetInfo
+from .pipelines import Compose
+from .samplers import DistributedSampler
+
+from .datasets import (  # isort:skip
+    TopDownCocoDataset,    
+    TopDownMpiiDataset, 
+    )
+
+__all__ = [
+    'TopDownCocoDataset', 'TopDownMpiiDataset'
+]
