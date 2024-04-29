7d5a984949a849406ce5b4d61c74843ad355d608
Hao Zhang <49357570+zhanghao5201@users.noreply.github.com>
2024-04-29 17:06:02 +0800

    Add files via upload

---
+++ tools/mmpose/models/heads/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
+from .topdown_heatmap_base_head import TopdownHeatmapBaseHead
+from .topdown_heatmap_simple_head import TopdownHeatmapSimpleHead
+
+
+__all__ = [
+    'TopdownHeatmapSimpleHead', 
+     'TopdownHeatmapBaseHead',
+]
